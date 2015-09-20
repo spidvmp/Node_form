8c274562563b4122752661e11d4ea55527fd2148
Vicente de Miguel <spidvmp@gmail.com>
2015-09-20 14:57:49 +0200

    Creo el lib/leeVersionDeModulo que leera el fich con los modulos y debera comparar con lo que se puso en el form

--- routes/version.js
+++ routes/version.js
@@ -9,7 +9,7 @@ he de leer el fichero package.josn para obtener los modulos que hay y las versio
  Asi que genero el modulo leeVersionDeModulo que se le pasa el nombre de un modulo y devuelve la version. En caso de que no lo tenga pues devuelve un JSOn vacio
  */
 
-//var leeVersionModulo = require('../lib/leeVersionDeModulo');
+var leeVersionModulo = require('../lib/leeVersionDeModulo');
 
 router.get('/', function(req,res){
     //recibimos la peticion de que queremos ller la dependencia
@@ -39,19 +39,12 @@ router.post('/buscar',function(req,res){
     console.log("envio a render ",datosJSON);
 
     //he de enviar un array, no un JSON, asi que transformo el json en array
-    var arr = Object.getOwnPropertyNames(datosJSON);
-    var arr1 = [modulo,'fff','jidsls'];
-    res.render('version',{data : arr1});
+    var arr = [];
+    arr.push(modulo);
+
+    res.render('version',{data : arr});
 });
 
 
 module.exports = router;
 
-router.post('/buscar',function(req,res){
-    //como me han hecho un form con un post, saco el dato de req.body.nombre de variable en el formulario
-    var modulo= req.body.nombremodulo;
-
-    //le añado 2 valroes mas para que el bucle tenga mas cosas para mostrar, solo deberia enviar [modulo]
-    var arr1 = [modulo,'fff','jidsls'];
-    res.render('version',{data : arr1});
-});
