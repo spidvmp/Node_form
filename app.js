d3c6b35b97a42dc459c92a86c0c4bc3b0d7add3e
Vicente de Miguel <spidvmp@gmail.com>
2015-09-20 11:45:22 +0200

    Heca la parte de que se genere la vista pero de momento sin ningun dato

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/version', version);
+app.use('/version', require('./routes/version'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/version.js
+++ routes/version.js
@@ -1,3 +1,26 @@
-/**
- * Created by vtx on 20/9/15.
+'use strict';
+var express=require('express');
+
+var router=express.Router();
+
+/*
+he de leer el fichero package.josn para obtener los modulos que hay y las versiones, pero lo vamos a llevar a otro modulo,
+ para que este sea mas sencillo. Hay que intentar que los modulos sena los mas sencillos posibles para que asi se puedan reutilizar
+ Asi que genero el modulo leeVersionDeModulo que se le pasa el nombre de un modulo y devuelve la version. En caso de que no lo tenga pues devuelve un JSOn vacio
  */
+
+//var leeVersionModulo = require('../lib/leeVersionDeModulo');
+
+router.get('/', function(req,res){
+    //recibimos la peticion de que queremos ller la dependencia
+
+    console.log('esto es req:' , req);
+
+    //esto devuelve a res que vista tiene que mostrar, en este caso version.ejs y no le pasa parametros
+    res.render('version',null);
+
+
+});
+
+
+module.exports = router;
